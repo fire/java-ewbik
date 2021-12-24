4550a5d6fa67caeec7961c10a0e66f6512eba323
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2021-12-24 10:03:15 -0800

    Restore constraints.

--- ewbIK/src/Humanoid_Manual.java
+++ ewbIK/src/Humanoid_Manual.java
@@ -143,95 +143,95 @@ public class Humanoid_Manual extends PApplet{
 	}
 
 	public void setBoneConstraints() {    
-		// dKusudama r_collar_joint = new dKusudama(r_collar_bone);
-		// r_collar_joint.addLimitConeAtIndex(0, new DVector(1.0, 0.4, 0), 0.7);
-		// r_collar_joint.setAxialLimits(-0.3d, 1d);
-		// r_collar_joint.optimizeLimitingAxes();
-		// r_collar_joint.setPainfullness(0.1d);
+		dKusudama r_collar_joint = new dKusudama(r_collar_bone);
+		r_collar_joint.addLimitConeAtIndex(0, new DVector(1.0, 0.4, 0), 0.7);
+		r_collar_joint.setAxialLimits(-0.3d, 1d);
+		r_collar_joint.optimizeLimitingAxes();
+		r_collar_joint.setPainfullness(0.1d);
 
-		// dKusudama r_shoulder = new dKusudama(r_upper_arm); 
-		// r_shoulder.addLimitConeAtIndex(0, new DVector(1, .9, 0.5), 1f);
-		// r_shoulder.addLimitConeAtIndex(1, new DVector(1, 1, 0.5), 1f);
-		// r_shoulder.setAxialLimits(-1.7d, 1.7d);
-		// r_shoulder.optimizeLimitingAxes();
-		// r_shoulder.setPainfullness(0.05d);
+		dKusudama r_shoulder = new dKusudama(r_upper_arm); 
+		r_shoulder.addLimitConeAtIndex(0, new DVector(1, .9, 0.5), 1f);
+		r_shoulder.addLimitConeAtIndex(1, new DVector(1, 1, 0.5), 1f);
+		r_shoulder.setAxialLimits(-1.7d, 1.7d);
+		r_shoulder.optimizeLimitingAxes();
+		r_shoulder.setPainfullness(0.05d);
 
-		// dKusudama r_elbow = new dKusudama(r_lower_arm);
-		// r_elbow.addLimitConeAtIndex(0, new DVector(0, -1, 0.1), 0.025);
-		// r_elbow.addLimitConeAtIndex(1, new DVector(0, 1, 0.1), 0.025);
-		// r_elbow.setAxialLimits(-2.7d, 2.7d);
-		// r_elbow.optimizeLimitingAxes();
+		dKusudama r_elbow = new dKusudama(r_lower_arm);
+		r_elbow.addLimitConeAtIndex(0, new DVector(0, -1, 0.1), 0.025);
+		r_elbow.addLimitConeAtIndex(1, new DVector(0, 1, 0.1), 0.025);
+		r_elbow.setAxialLimits(-2.7d, 2.7d);
+		r_elbow.optimizeLimitingAxes();
 
-		// dKusudama r_wrist = new dKusudama(r_hand);
-		// r_wrist.addLimitConeAtIndex(0, new DVector(0, 0.7, -0.7), Math.toRadians(45d));
-		// r_wrist.addLimitConeAtIndex(1, new DVector(0, 0.7, 0.7), Math.toRadians(45d));
-		// r_wrist.setAxialLimits(-0.01d, 0.02d);
-		// r_wrist.optimizeLimitingAxes();
+		dKusudama r_wrist = new dKusudama(r_hand);
+		r_wrist.addLimitConeAtIndex(0, new DVector(0, 0.7, -0.7), Math.toRadians(45d));
+		r_wrist.addLimitConeAtIndex(1, new DVector(0, 0.7, 0.7), Math.toRadians(45d));
+		r_wrist.setAxialLimits(-0.01d, 0.02d);
+		r_wrist.optimizeLimitingAxes();
 
-		// dKusudama l_collar_joint = new dKusudama(l_collar_bone);
-		// l_collar_joint.addLimitConeAtIndex(0, new DVector(-1.0, 0.4, 0), 0.7);
-		// l_collar_joint.setAxialLimits(-0.7d, 1d);
-		// l_collar_joint.optimizeLimitingAxes();
-		// l_collar_joint.setPainfullness(0.1d);
+		dKusudama l_collar_joint = new dKusudama(l_collar_bone);
+		l_collar_joint.addLimitConeAtIndex(0, new DVector(-1.0, 0.4, 0), 0.7);
+		l_collar_joint.setAxialLimits(-0.7d, 1d);
+		l_collar_joint.optimizeLimitingAxes();
+		l_collar_joint.setPainfullness(0.1d);
 
-		// dKusudama l_shoulder = new dKusudama(l_upper_arm); 
-		// l_shoulder.addLimitConeAtIndex(0, new DVector(-1, .9, 0.5), 1f);
-		// l_shoulder.addLimitConeAtIndex(1, new DVector(-1, 1, 0.5), 1f);
-		// l_shoulder.setAxialLimits(-.3d, 1.7d);
-		// l_shoulder.optimizeLimitingAxes();
-		// l_shoulder.setPainfullness(0.05d);
+		dKusudama l_shoulder = new dKusudama(l_upper_arm); 
+		l_shoulder.addLimitConeAtIndex(0, new DVector(-1, .9, 0.5), 1f);
+		l_shoulder.addLimitConeAtIndex(1, new DVector(-1, 1, 0.5), 1f);
+		l_shoulder.setAxialLimits(-.3d, 1.7d);
+		l_shoulder.optimizeLimitingAxes();
+		l_shoulder.setPainfullness(0.05d);
 
-		// dKusudama l_elbow = new dKusudama(l_lower_arm);
-		// l_elbow.addLimitConeAtIndex(0, new DVector(0, -1, 0.1), 0.025);
-		// l_elbow.addLimitConeAtIndex(1, new DVector(0, 1, 0.1), 0.025);
-		// l_elbow.setAxialLimits(-0.0d, 2.7d);
-		// l_elbow.optimizeLimitingAxes();
+		dKusudama l_elbow = new dKusudama(l_lower_arm);
+		l_elbow.addLimitConeAtIndex(0, new DVector(0, -1, 0.1), 0.025);
+		l_elbow.addLimitConeAtIndex(1, new DVector(0, 1, 0.1), 0.025);
+		l_elbow.setAxialLimits(-0.0d, 2.7d);
+		l_elbow.optimizeLimitingAxes();
 
-		// dKusudama l_wrist = new dKusudama(l_hand);
-		// l_wrist.addLimitConeAtIndex(0, new DVector(0, 0.7, -0.7), Math.toRadians(45d));
-		// l_wrist.addLimitConeAtIndex(1, new DVector(0, 0.7, 0.7), Math.toRadians(45d));
-		// l_wrist.setAxialLimits(0.01d, 0.02d);
-		// l_wrist.optimizeLimitingAxes();
+		dKusudama l_wrist = new dKusudama(l_hand);
+		l_wrist.addLimitConeAtIndex(0, new DVector(0, 0.7, -0.7), Math.toRadians(45d));
+		l_wrist.addLimitConeAtIndex(1, new DVector(0, 0.7, 0.7), Math.toRadians(45d));
+		l_wrist.setAxialLimits(0.01d, 0.02d);
+		l_wrist.optimizeLimitingAxes();
 
-		// dKusudama neck1j = new dKusudama(neck_1);
-		// neck1j.addLimitConeAtIndex(0, new DVector(0,1,0), 0.01d);
-		// neck1j.setAxialLimits(0.001d, 0.002d);
-		// neck1j.optimizeLimitingAxes();		
+		dKusudama neck1j = new dKusudama(neck_1);
+		neck1j.addLimitConeAtIndex(0, new DVector(0,1,0), 0.01d);
+		neck1j.setAxialLimits(0.001d, 0.002d);
+		neck1j.optimizeLimitingAxes();		
 
-		// dKusudama c1j = new dKusudama(c1);
-		// c1j.addLimitConeAtIndex(0, new DVector(0,1,0), Math.toRadians(10d));
-		// c1j.setAxialLimits(-Math.toRadians(10d), Math.toRadians(20d));
-		// c1j.optimizeLimitingAxes();
+		dKusudama c1j = new dKusudama(c1);
+		c1j.addLimitConeAtIndex(0, new DVector(0,1,0), Math.toRadians(10d));
+		c1j.setAxialLimits(-Math.toRadians(10d), Math.toRadians(20d));
+		c1j.optimizeLimitingAxes();
 
-		// dKusudama c2j = new dKusudama(c2);
-		// c2j.addLimitConeAtIndex(0, new DVector(0,1,0), Math.toRadians(10d));
-		// c2j.setAxialLimits(-Math.toRadians(10d), Math.toRadians(20d));
-		// c2j.optimizeLimitingAxes();
+		dKusudama c2j = new dKusudama(c2);
+		c2j.addLimitConeAtIndex(0, new DVector(0,1,0), Math.toRadians(10d));
+		c2j.setAxialLimits(-Math.toRadians(10d), Math.toRadians(20d));
+		c2j.optimizeLimitingAxes();
 
-		// dKusudama c3j = new dKusudama(c3);
-		// c3j.addLimitConeAtIndex(0, new DVector(0,1,0), Math.toRadians(10d));
-		// c3j.setAxialLimits(-Math.toRadians(10d), Math.toRadians(20d));
-		// c3j.optimizeLimitingAxes();
+		dKusudama c3j = new dKusudama(c3);
+		c3j.addLimitConeAtIndex(0, new DVector(0,1,0), Math.toRadians(10d));
+		c3j.setAxialLimits(-Math.toRadians(10d), Math.toRadians(20d));
+		c3j.optimizeLimitingAxes();
 
-		// dKusudama c4j = new dKusudama(c4);
-		// c4j.addLimitConeAtIndex(0, new DVector(0,1,0), Math.toRadians(10d));
-		// c4j.setAxialLimits(-Math.toRadians(10d), Math.toRadians(20d));
-		// c4j.optimizeLimitingAxes();
+		dKusudama c4j = new dKusudama(c4);
+		c4j.addLimitConeAtIndex(0, new DVector(0,1,0), Math.toRadians(10d));
+		c4j.setAxialLimits(-Math.toRadians(10d), Math.toRadians(20d));
+		c4j.optimizeLimitingAxes();
 
-		// dKusudama c5j = new dKusudama(c5);
-		// c5j.addLimitConeAtIndex(0, new DVector(0,1,0), Math.toRadians(10d));
-		// c5j.setAxialLimits(-Math.toRadians(10d), Math.toRadians(20d));
-		// c5j.optimizeLimitingAxes();
+		dKusudama c5j = new dKusudama(c5);
+		c5j.addLimitConeAtIndex(0, new DVector(0,1,0), Math.toRadians(10d));
+		c5j.setAxialLimits(-Math.toRadians(10d), Math.toRadians(20d));
+		c5j.optimizeLimitingAxes();
 
-		// dKusudama neck2j = new dKusudama(neck_2);
-		// neck2j.addLimitConeAtIndex(0, new DVector(0,1,0), Math.toRadians(10d));
-		// neck2j.setAxialLimits(-Math.toRadians(10d), Math.toRadians(20d));
-		// neck2j.optimizeLimitingAxes();
+		dKusudama neck2j = new dKusudama(neck_2);
+		neck2j.addLimitConeAtIndex(0, new DVector(0,1,0), Math.toRadians(10d));
+		neck2j.setAxialLimits(-Math.toRadians(10d), Math.toRadians(20d));
+		neck2j.optimizeLimitingAxes();
 
-		// dKusudama headj = new dKusudama(head);
-		// headj.addLimitConeAtIndex(0, new DVector(0,1,0), Math.toRadians(40d));
-		// headj.setAxialLimits(-Math.toRadians(10d), Math.toRadians(20d));
-		// headj.optimizeLimitingAxes();
+		dKusudama headj = new dKusudama(head);
+		headj.addLimitConeAtIndex(0, new DVector(0,1,0), Math.toRadians(40d));
+		headj.setAxialLimits(-Math.toRadians(10d), Math.toRadians(20d));
+		headj.optimizeLimitingAxes();
 	}
 
 
